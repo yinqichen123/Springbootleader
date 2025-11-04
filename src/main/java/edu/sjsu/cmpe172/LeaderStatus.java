f4bf23efad1803b2988fc0e72e9af482651233ea
yinqichen123 <yinqi.chen@sjsu.edu>
2025-11-04 09:30:49 -0800

    Update LeaderStatus.java

--- src/main/java/edu/sjsu/cmpe172/LeaderStatus.java
+++ src/main/java/edu/sjsu/cmpe172/LeaderStatus.java
@@ -1,6 +1,6 @@
 package edu.sjsu.cmpe172;
 
-public enum LeaderStatus {
+public enum LeaderStatus {            // reference:https://www.baeldung.com/java-enum-simple-state-machine
     WATCHING,   // Not interested in being leader
     WAITING,    // Want to be leader but someone else is
     LEADING     // Currently the leader
